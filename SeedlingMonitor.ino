1dafedf94bcfd8d2218e1f3a499dd3f9aac1214b
Sigbjørn Trageton <sigbjorn@trageton.net>
2019-03-24 14:18:08 +0100

    Added a Nokia 51100 lcd for display
    Removed serial printing
    Added PCDj8544 library for lcd
    Created custom glyph for degrees symbol
    Use simpleTimer to do the readings

--- SeedlingMonitor.ino
+++ SeedlingMonitor.ino
@@ -1,29 +1,43 @@
 #include <SimpleTimer.h>
 #include <DHT.h>
+#include <PCD8544.h>
 
 #define DHTTYPE DHT22
 
-const short tempHumSensorPin = 4;
+const short tempHumSensorPin = 8;
 
 DHT dht(tempHumSensorPin, DHTTYPE);
+SimpleTimer timer;
+PCD8544 lcd;
+static const byte degrees_glyph[] = { 0x00, 0x07, 0x05, 0x07, 0x00 };
 
-void setup(void) {
-   delay(1000);
+void setup() {
    dht.begin();
-
-   Serial.begin(115200);
+   lcd.begin(84, 48);
+   lcd.setContrast(60);
+   lcd.createChar(0, degrees_glyph);
+   lcd.clear();
+   lcd.setCursor(0, 0);
+   lcd.print("Seedling controller");
+   delay(5000);
+   lcd.clear();
    timer.setInterval(10000, readSensorValues);
 }
 
 void readSensorValues() {
-   float t = dht.readTemperature();
-   float h = dht.readHumidity();
-   Serial.print("Temperature = ");
-   Serial.print(t);
-   Serial.print(" *C ");
-   Serial.print("    Humidity = ");
-   Serial.print(h);
-   Serial.println(" % ");
+   float temp = dht.readTemperature();
+   float hum = dht.readHumidity();
+
+   lcd.setCursor(0,0);
+   lcd.print("Temp: ");
+   lcd.print(temp, 1);
+   lcd.print(" ");
+   lcd.write(0);
+   lcd.print("C ");
+   lcd.setCursor(0,1);
+   lcd.print("Fukt: ");
+   lcd.print(hum, 1);
+   lcd.print(" % ");
 }
 
 void loop(void) {
