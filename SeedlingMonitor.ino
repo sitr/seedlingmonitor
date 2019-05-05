b4185af3981ac289bbb44fe8964c872614349f34
Sigbjørn Trageton <sigbjorn@trageton.net>
2019-05-05 22:36:36 +0200

    Added lcd, pH sensor and splash screen
    Support for Nokia 5110 LCD
    Collect readings from pH sensor ph-4502C
    Splash screen with logo

--- SeedlingMonitor.ino
+++ SeedlingMonitor.ino
@@ -1,45 +1,99 @@
 #include <SimpleTimer.h>
 #include <DHT.h>
-#include <PCD8544.h>
+#include <LCD5110_Graph.h>
+#include <x10rf.h>
 
 #define DHTTYPE DHT22
 
-const short tempHumSensorPin = 8;
+extern uint8_t splashScreen[];
+const short lcdPinCLK = 8;
+const short lcdPinDIN = 9;
+const short lcdPinDC = 10;
+const short lcdPinRST = 11;
+const short lcdPinCE = 12;
+
+const short tempHumSensorPin = 1;
+const short phSensorPin = A0;
+const short txPin = 2;
+const short reps = 1;
+
+extern uint8_t SmallFont[];
+extern uint8_t TinyFont[];
 
 DHT dht(tempHumSensorPin, DHTTYPE);
 SimpleTimer timer;
-PCD8544 lcd;
+LCD5110 lcd(lcdPinCLK, lcdPinDIN, lcdPinDC, lcdPinRST, lcdPinCE);
+x10rf myx10 = x10rf(txPin,13,reps);
+
 static const byte degrees_glyph[] = { 0x00, 0x07, 0x05, 0x07, 0x00 };
 
 void setup() {
    dht.begin();
-   lcd.begin(84, 48);
-   lcd.setContrast(60);
-   lcd.createChar(0, degrees_glyph);
-   lcd.clear();
-   lcd.setCursor(0, 0);
-   lcd.print("Seedling controller");
+   myx10.begin();
+   lcd.InitLCD();
+   lcd.drawBitmap(0, 0, splashScreen, 84, 48);
+   lcd.update();
    delay(5000);
-   lcd.clear();
-   timer.setInterval(10000, readSensorValues);
+   lcd.clrScr();
+   lcd.setFont(SmallFont);
+
+   readSensorValues();
+   timer.setInterval(3000, readSensorValues);
 }
 
 void readSensorValues() {
-   float temp = dht.readTemperature();
-   float hum = dht.readHumidity();
+   float temperature = dht.readTemperature();
+   float humidity = dht.readHumidity();
+   float ph = readPhValue();
+   delay(20);
 
-   lcd.setCursor(0,0);
-   lcd.print("Temp: ");
-   lcd.print(temp, 1);
-   lcd.print(" ");
-   lcd.write(0);
-   lcd.print("C ");
-   lcd.setCursor(0,1);
-   lcd.print("Fukt: ");
-   lcd.print(hum, 1);
-   lcd.print(" % ");
+   String tempString = "Tmp: " + String(temperature, 1) + " C";
+   String humString = "Hum: " + String(humidity, 1) + "%";
+   String phString = "pH:  " + String(ph, 1);
+
+   lcd.print(tempString, 0, 0);
+   lcd.print(humString, 0, 12);
+   lcd.print(phString, 0, 24);
+   lcd.update();
+  // sendData(temperature, humidity);
 }
 
-void loop(void) {
+float readPhValue() {
+   unsigned long int avgPhValue;
+   int buf[10], temp;
+   for(int i = 0; i < 10; i++)
+   {
+      buf[i]=analogRead(phSensorPin);
+      delay(10);
+   }
+   for(int i = 0; i < 9; i++)
+      {
+      for(int j = i + 1; j < 10; j++)
+      {
+         if(buf[i] > buf[j])
+         {
+            temp=buf[i];
+            buf[i]=buf[j];
+            buf[j]=temp;
+         }
+      }
+   }
+   avgPhValue = 0;
+   for(int i = 2; i < 8; i++)
+   avgPhValue += buf[i];
+   float pHVol= (float)avgPhValue * 5.0 / 1024 / 6;
+   float phValue = -5.70 * pHVol + 21.34;
+
+   return phValue;
+}
+
+void sendData(float temperature, float humidity){
+   myx10.RFXsensor(1, 't', 't', temperature);
+   delay(250);
+   myx10.RFXsensor(2, 't', 'h', humidity);
+   delay(250);
+}
+
+void loop() {
    timer.run();
 }
